275b919b221ac0f6dd0bd9bf38a042739700d491
Emily Owaki <emily.owaki@gmail.com>
2014-12-28 11:34:11 -0600

    adding email to advocate migration and starting on validations for advocate and connections

--- app/models/advocate.rb
+++ app/models/advocate.rb
@@ -1,4 +1,7 @@
 class Advocate < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, uniqueness: true
+
   has_many  :connections
   has_many  :opportunities, through: :connections
   has_many  :actions
--- db/migrate/20141226022527_create_advocates.rb
+++ db/migrate/20141226022527_create_advocates.rb
@@ -2,8 +2,10 @@ class CreateAdvocates < ActiveRecord::Migration
   def change
     create_table :advocates do |t|
       t.string  :name
+      t.string  :email
       t.string  :twitter
       t.string  :blog
+
       t.timestamps
     end
   end
