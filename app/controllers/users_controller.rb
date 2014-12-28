64976bbb61ea27f3b48de08579d9e6b621b4dc43
johnw335 <jww335@gmail.com>
2014-12-28 14:03:00 -0600

    added login form and create route for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+  def index
+  end
+
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_url
+    else
+      flash.now[:error] = "Could not signup.  Please fill in all fields"
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email, :passowrd)
+    end
+
 end
