1173176b8d8599efa8c3f7164ffb8ac3d52d9d4e
Emily Owaki <emily.owaki@gmail.com>
2014-12-28 21:32:33 -0600

    uhhh not really sure what to say in this commit except that I think I broke rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users
-  resources :sessions, only:[:new, :create, :destroy]
+  resources :sessions
   resources :opportunities do
     resources :ctos
   end
