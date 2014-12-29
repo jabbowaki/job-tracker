d197e7d84c86ab32ac552cc0f8c37c6c89ec831c
Emily Owaki <emily.owaki@gmail.com>
2014-12-28 21:16:16 -0600

    oh my god, the hydra rears its head again. working on fixing that, in the mean time there is now a login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      @error_message = "Invalid email or password"
+      render "sessions/new"
+    end
+  end
+
+  def destroy
+  end
+end
