24c853e2d756a1b50569bdd549c11ceef69b168d
johnw335 <jww335@gmail.com>
2014-12-29 19:14:39 -0600

    strted work on the opportunities index and show

--- app/controllers/opportunities_controller.rb
+++ app/controllers/opportunities_controller.rb
@@ -1,2 +1,11 @@
 class OpportunitiesController < ApplicationController
+
+  def index
+    @user = User.find(session[:user_id])
+    @opportunities = @user.opportunities
+  end
+
+  def show
+    @opportunity = Opportunity.find(params[:id])
+  end
 end
