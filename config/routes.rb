307d0ee00e7d14ae163325b22cff0d3b9c6afce5
Emily Owaki <emily.owaki@gmail.com>
2015-01-04 10:24:08 -0600

    fixing tabbing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :opportunities do
-  resources :advocates
+    resources :advocates
     resources :actions
     resources :ctos
   end
