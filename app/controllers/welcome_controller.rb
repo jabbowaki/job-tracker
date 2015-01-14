771f3b446d516508a20ee91fc592f0b8d85e9c5a
Emily Owaki <emily.owaki@gmail.com>
2015-01-14 10:48:33 -0600

    removing commented out/older method

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,9 @@
 class WelcomeController < ApplicationController
 
   def index
-
-    # @user = User.find(session[:user_id])
     if session[:user_id]
       redirect_to opportunities_path
     end
-
   end
+
 end
