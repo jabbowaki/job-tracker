2ffc361c989bc465ed1c581c301556cdc29be2e0
Emily Owaki <emily.owaki@gmail.com>
2015-01-17 19:21:30 -0600

    adding initial tests for sign up and login

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -1,4 +1,4 @@
-require 'rails-helper'
+require 'rails_helper'
 
 feature "signin" do
   before do
@@ -13,4 +13,27 @@ feature "signin" do
     click_on 'Login'
     expect(page).to have_text('Logout')
   end
+
+  scenario "when user logs in with in invalid params" do
+    visit '/'
+    click_on 'Login'
+    fill_in 'email', :with => "test@gmail.com"
+    fill_in 'password', :with => 'testing124'
+    click_on 'Login'
+    expect(page).to have_text('Login')
+  end
+end
+
+feature "sign up" do
+  scenario "when user signs up with valid params" do
+    visit '/'
+    click_on 'Sign Up'
+    fill_in 'First Name', :with => "Ryan"
+    fill_in 'Last Name', :with => "Gosling"
+    fill_in 'Email', :with => "ryan@gmail.com"
+    fill_in 'Password', :with => 'omgryangosling'
+    click_on 'Sign Up'
+    expect(page).to have_text('Logout')
+  end
+
 end
