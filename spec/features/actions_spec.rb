abf27a7834679a9892fe3683c8bb6cd342786c28
Emily Owaki <emily.owaki@gmail.com>
2015-01-22 20:55:17 -0600

    adding recent action test

--- spec/features/actions_spec.rb
+++ spec/features/actions_spec.rb
@@ -44,3 +44,32 @@ feature 'new actions' do
     expect(page).to have_content("Add a new action")
   end
 end
+
+feature 'view actions' do
+  before do
+    @user = User.create!(first_name: "test", last_name: "person", email: "test@gmail.com", password: "testing123")
+    @opp = Opportunity.create!(user: @user, name: "Burger King", website: "www.burgerking.com", career_page: "www.burgerking.com/jobs")
+    @advocate = Advocate.create!(name: "Ronald McDonald", email: "ronald@mcdonalds.com", twitter: "@ronaldmcdonald", opportunity: @opp)
+    @action = Action.create!(description: "Phone Interview", opportunity: @opp, advocate: @advocate, meeting_date: "2014-12-10")
+
+
+  end
+  scenario 'existing actions show up on opp show page' do
+    visit root_path
+    click_on 'Login'
+    fill_in 'email', :with => "test@gmail.com"
+    fill_in 'password', :with => 'testing123'
+    click_on 'Login'
+    click_on 'Burger King'
+    expect(page).to have_content("Phone Interview")
+  end
+
+  scenario 'recent actions show actions in the past' do
+    visit root_path
+    click_on 'Login'
+    fill_in 'email', :with => "test@gmail.com"
+    fill_in 'password', :with => 'testing123'
+    click_on 'Login'
+    expect(page).to have_content("Recent Activity Phone Interview on 2014-12-10" )
+  end
+end
