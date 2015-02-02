f9e4dd28e12d80a07aaf708db64f40ebe98fc0da
Emily Owaki <emily.owaki@gmail.com>
2015-02-02 09:51:20 -0600

    adding new and create methods

--- app/controllers/ctos_controller.rb
+++ app/controllers/ctos_controller.rb
@@ -1,2 +1,21 @@
 class CtosController < ApplicationController
+  def new
+    @opportunity = Opportunity.find(params[:opportunity_id])
+    @cto = Cto.new
+  end
+
+  def create
+    @opportunity = Opportunity.find(params[:opportunity_id])
+    @cto = Cto.new(cto_params)
+    if @cto.save
+      redirect_to opportunity_path(@opportunity)
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def cto_params
+      params.require(:cto).permit(:name, :email, :twitter, :blog)
+    end
 end
