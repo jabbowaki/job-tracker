1d121b1e84a72fe2992a69911949a6bc910abc29
Emily Owaki <emily.owaki@gmail.com>
2015-02-02 11:22:28 -0600

    work in progress, first test is having issue where it isn't exactly equal to the object but returns the correct one

--- spec/controllers/cto_controller_spec.rb
+++ spec/controllers/cto_controller_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
 
-describe 'CTO controller' do
+describe CtosController do
   let!(:user) {User.create!(first_name: "Test", last_name: "McTest", email: "test@gmail.com", password: "thisisatest")}
+  let!(:opp) {Opportunity.create!(user: user, name: "Average company", website: "www.average.com")}
 
   describe 'GET #new' do
-    it 'assigns @opportunity to specific opportunity'
-
-
+    it 'assigns @opportunity to specific opportunity' #do
+    #   get :new, { opportunity_id: opp.to_param }
+    #   expect(assigns(:opportunity)).to equal(opp)
+    # end
   end
 
   describe 'POST #create' do
     it 'assigns @opportunity to a specific opp'
+
     it 'assigns @cto a new, unsaved cto'
     it 'saved cto when passed valid params'
     it 'fails to save cto when passed invalid params'
